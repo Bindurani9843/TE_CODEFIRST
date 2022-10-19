f73ea7b2986cb8db30207887d360ad38383b0c89
Bindu <bindurani9843@gmail.com>
2022-10-19 18:00:35 +0530

    Add project files.

---
+++ TE_CODEFIRST/Models/TraineeDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace TE_CODEFIRST.Models
+{
+    public class TraineeDbContext : DbContext
+    {
+        public TraineeDbContext(DbContextOptions<TraineeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Trainee> Trainees { get; set; }
+
+    }
+}
